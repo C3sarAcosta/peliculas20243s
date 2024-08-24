720043a8d54b2a3ed892f20831fba45806283137
Cesar Acosta <c3sar.adan@gmail.com>
2024-08-24 16:22:05 -0600

    24/08/2024 Parte 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'provider/movies_provider.dart';
 import 'screens/screens.dart';
 
 void main() => runApp(const MyApp());
