b322c69dd589a15d1346df685580d4038fac89f7
Cesar Acosta <c3sar.adan@gmail.com>
2024-08-24 16:57:14 -0600

    24/08/2024 Parte 6

--- lib/provider/movies_provider.dart
+++ lib/provider/movies_provider.dart
@@ -2,11 +2,16 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:peliculas20243s/models/now_playing_response.dart';
+
+import '../models/movie.dart';
 
 class MoviesProvider extends ChangeNotifier {
   String _baseUrl = 'api/themoviedb.org';
   String _apiKey = '9dc27117b000e7e5acfb365fa957971a';
   String _language = 'es-MX';
+
+  List<Movie> onDisplayMovies = [];
 
   MoviesProvider() {
     getOnDisplayMovies();
@@ -18,7 +23,10 @@ class MoviesProvider extends ChangeNotifier {
 
     final response = await http.get(url);
     final Map<String, dynamic> decodeData = json.decode(response.body);
-    print(decodeData);
-    print(response.body);
+    //print(decodeData);
+    //print(response.body);
+    final onNowPlayingResponse = NowPlayingResponse.fromRawJson(response.body);
+    notifyListeners();
+    print(onNowPlayingResponse.results[0].title);
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../provider/movies_provider.dart';
 import '../widgets/card_swiper.dart';
 import '../widgets/movie_slider.dart';
 
@@ -8,6 +10,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final moviesProvider = Provider.of<MoviesProvider>(context);
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -23,7 +26,7 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          CardSwiper(),
+          CardSwiper(movies: moviesProvider.onDisplayMovies),
           MovieSlider(),
         ],
       ),
